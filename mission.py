602dfa7e56b90918b7362b14e61bb6569bc7fbb5
Hussein Fuseini <fuseinial@gmail.com>
2025-03-27 12:33:18 +0000

    add db name and db_engine to code

--- mission.py
+++ mission.py
@@ -3,8 +3,15 @@ class Mission:
         self._name = name
         self._description = description
         self.mission_informations = f"Mission\nName: {self._name}\nDescription:{self._description}"
-
-
+        self.db_name = ""
+        self.db_engine = ""
+    def set_db_name(self, name):
+        self.db_name = name
+    def set_db_engine(self, engine):
+        self.db_engine = engine
+    def __str__(self):
+        return self.mission_informations
 if __name__ == "__main__":
     mission = Mission("Alien Invasion", "Destroy the alien planet and save humanity")
-    print(mission.mission_informations)
+    db_name = "******"
+    print(mission)
